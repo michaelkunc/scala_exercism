d41957d2cf6967b691d1f9b68c7db0ff48b6d9fe
michaelkunc <mjkunc@gmail.com>
2016-07-15 14:11:11 -0700

    is this thing on?

--- hello-world/src/main/scala/helloworld.scala
+++ hello-world/src/main/scala/helloworld.scala
@@ -1,7 +1,3 @@
 object HelloWorld {
-  def hello(name: String = "World") = "Hello, " + name +"!";
-
-  def main(args: Array[String]) :Unit = {
-    println(hello())
-  }
+  def hello(name: String = "World") = s"Hello, ${name}!";
 }
