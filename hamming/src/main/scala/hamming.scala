876408247c41c05862b3a63fa64b57ead5f2a39b
michaelkunc <mjkunc@gmail.com>
2016-07-21 14:36:36 -0700

    refactor to hamming solution

--- hamming/src/main/scala/hamming.scala
+++ hamming/src/main/scala/hamming.scala
@@ -1,10 +1,7 @@
 object Hamming {
 
   def compute(strand1: String, strand2 : String): Int ={
-    if (strand1.length != strand2.length) {
-      throw new IllegalArgumentException("Strands must be the same length")
-    } else {
-      (strand1.zip(strand2).count(x => x._1 != x._2))
-    }
+    Predef.require(strand1.length == strand2.length, "Strands must be the same length")
+    (strand1.zip(strand2).count(x => x._1 != x._2))
   }
 }
