af85b71b3c11d49284bd2e8c74a83abf56e66069
michaelkunc <mjkunc@gmail.com>
2016-07-26 17:45:52 -0700

    word count passing

--- word-count/src/main/scala/word_count.scala
+++ word-count/src/main/scala/word_count.scala
@@ -1,6 +1,13 @@
+import collection.mutable
+
 class Phrase(phrase : String){
 
-  def wordCount(): Map[String, Int] = {
-    Map("word" -> 1)
+  def wordCount(): mutable.Map[String, Int] = {
+    val counts = mutable.Map.empty[String, Int].withDefaultValue(0)
+    for (words <- this.phrase.split("[^\\w']+")){
+      val word = words.toLowerCase
+      counts(word) += 1
+    }
+    counts
   }
 }
