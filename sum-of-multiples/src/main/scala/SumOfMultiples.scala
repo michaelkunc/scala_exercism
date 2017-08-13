b0e29507909a6a268e9c3aa678f7815ae8d1800a
michaelkunc <mjkunc@gmail.com>
2017-08-13 11:16:38 -0700

    refactored solution

--- sum-of-multiples/src/main/scala/SumOfMultiples.scala
+++ sum-of-multiples/src/main/scala/SumOfMultiples.scala
@@ -1,17 +1,8 @@
 object SumOfMultiples {
   def sumOfMultiples(factors: Set[Int], limit: Int): Int = 
-	  if (factors.isEmpty || limit < factors.head){
-	  	0
-	  } else {
-	  	val l = factors.toSeq
-	  	var sum = 0
-	  	if (l.size == 2) { 
-		  	for( i <- l(0) until limit if i % l(0) == 0 || i % l(1) == 0) sum += i
-		  	sum
-	    } else {
-	    	for( i <- l(0) until limit if i % l(0) == 0 || i % l(1) == 0 || i % l(2) == 0) sum += i
-		  	sum
-	    }
-	  }  
+  (1 until limit)
+  	.filter(x => factors
+  		.exists(i => x % i == 0))
+  			.sum
 }
 
