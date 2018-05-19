8e69c9550fa3b64336f55d060b0acbfc1723810b
michaelkunc <mjkunc@gmail.com>
2018-05-19 07:09:02 -0700

    clean up commit

--- word-count/src/main/scala/word_count.scala
+++ word-count/src/main/scala/word_count.scala
@@ -1,13 +1,11 @@
-import collection.mutable
 
 class Phrase(phrase : String){
 
-  def wordCount(): mutable.Map[String, Int] = {
-    val counts = mutable.Map.empty[String, Int].withDefaultValue(0)
-    for (words <- this.phrase.split("[^\\w']+")){
-      val word = words.toLowerCase
-      counts(word) += 1
+  def wordCount(): Map[String, Int] = {
+    phrase
+      .toLowerCase
+      .split("[^\\w']+")
+      .groupBy(identity)
+      .mapValues(_.size)
     }
-    counts
-  }
 }
