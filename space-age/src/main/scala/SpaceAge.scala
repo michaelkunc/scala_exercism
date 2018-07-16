9a0d1629d005a4cb1a8fa3ab113bbf7b8547df61
michaelkunc <mjkunc@gmail.com>
2018-07-16 08:11:41 -0700

    revised space-age solution

--- space-age/src/main/scala/SpaceAge.scala
+++ space-age/src/main/scala/SpaceAge.scala
@@ -6,14 +6,25 @@ object SpaceAge {
     Math.round(thisAge * 100.0) / 100.0
     
   }
+
+  val planets = Map(
+    "Earth" -> 1.0,
+    "Mercury" -> 0.2408467,
+    "Venus" -> 0.61519726,
+    "Mars" -> 1.8808158,
+    "Jupiter" -> 11.862615,
+    "Saturn" -> 29.447498,
+    "Uranus" -> 84.016846,
+    "Neptune" -> 164.79132
+  )
   
 
-  def onEarth(age: Double): Double = getAge(age, 1)
-  def onMercury(age: Double): Double = getAge(age, 0.2408467)
-  def onVenus(age: Double): Double = getAge(age, 0.61519726) 
-  def onMars(age: Double): Double = getAge(age, 1.8808158)
-  def onJupiter(age: Double): Double = getAge(age, 11.862615)
-  def onSaturn(age: Double): Double = getAge(age, 29.447498)
-  def onUranus(age: Double): Double = getAge(age, 84.016846)
-  def onNeptune(age: Double): Double = getAge(age, 164.79132)
+  def onEarth(age: Double): Double = getAge(age, planets("Earth"))
+  def onMercury(age: Double): Double = getAge(age, planets("Mercury"))
+  def onVenus(age: Double): Double = getAge(age, planets("Venus")) 
+  def onMars(age: Double): Double = getAge(age, planets("Mars"))
+  def onJupiter(age: Double): Double = getAge(age, planets("Jupiter"))
+  def onSaturn(age: Double): Double = getAge(age, planets("Saturn"))
+  def onUranus(age: Double): Double = getAge(age, planets("Uranus"))
+  def onNeptune(age: Double): Double = getAge(age, planets("Neptune"))
 }
