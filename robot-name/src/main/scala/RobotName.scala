8958b9a2b333868c78f81b907496a0cb5691ff62
michaelkunc <mjkunc@gmail.com>
2018-08-06 08:38:45 -0700

    one more test passing

--- robot-name/src/main/scala/RobotName.scala
+++ robot-name/src/main/scala/RobotName.scala
@@ -1,7 +1,18 @@
 class Robot {
 
-    def name() = "AB123"
+    var name = name_builder()
 
-    def reset() = "AB123"
+    def reset() = name_builder()
+
+    def name_builder() = {
+        val r = scala.util.Random
+        val prefix = r.shuffle(('A' to 'Z').toList).take(2).mkString
+        val suffix = r.shuffle((0 to 9).toList).take(3).mkString
+        prefix + suffix
+    }
+    // need to
+    // 1) build the name
+    // 2) store it someplace
+    // 3) make sure names don't clash
 
 }
