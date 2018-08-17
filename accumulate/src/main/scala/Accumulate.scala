e535636d7c73b0580fa20dfb7d10abdaf1dfa5a7
michaelkunc <mjkunc@gmail.com>
2018-08-17 08:12:50 -0700

    accumulate solution

--- accumulate/src/main/scala/Accumulate.scala
+++ accumulate/src/main/scala/Accumulate.scala
@@ -1,6 +1,5 @@
 class Accumulate {
   def accumulate[A, B](f: (A) => B, list : List[A]): List[B] = {
-    B
-    // something like for (e <- i) yield e * e returns a List of Squares
+    for (e <- list) yield f(e)
   }
 }
