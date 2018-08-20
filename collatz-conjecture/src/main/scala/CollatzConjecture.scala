783c6e69f3adf86bdc4b3959a529ea355ccdd586
michaelkunc <mjkunc@gmail.com>
2018-08-20 05:56:38 -0700

    collatz conjecture solution

--- collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,7 +1,9 @@
 object CollatzConjecture{
     def steps(num: Int, stepCount: Int = 0): Option[Int] = num match {
+        case x if x < 1 => None
         case 1 => Some(stepCount)
-        case _ if num % 2 == 0 => (steps(num / 2, stepCount+1))
+        case x if x  % 2 == 0 => (steps(num / 2, stepCount+1))
+        case _ => (steps((num * 3) + 1, stepCount+1))
 
     }
 }
--- collatz-conjecture/src/test/scala/CollatzConjectureTest.scala
+++ collatz-conjecture/src/test/scala/CollatzConjectureTest.scala
@@ -13,22 +13,22 @@ class CollatzConjectureTest extends FunSuite with Matchers {
   }
 
   test("even and odd steps") {
-    pending
+    // pending
     CollatzConjecture.steps(12) should be (Some(9))
   }
 
   test("Large number of even and odd steps") {
-    pending
+    // pending
     CollatzConjecture.steps(1000000) should be (Some(152))
   }
 
   test("zero is an error") {
-    pending
+    // pending
     CollatzConjecture.steps(0) should be (None)
   }
 
   test("negative value is an error") {
-    pending
+    // pending
     CollatzConjecture.steps(-15) should be (None)
   }
 }
